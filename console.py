173aaa0434f896ba32afb82099054e51c0c62234
CHRISKINUNGI <chrskinungi@gmail.com>
2023-08-15 15:53:13 +0300

    commit to correct base_model.py file_storage.py impliment save() reload()

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
     consol
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -27,6 +29,62 @@ class HBNBCommand(cmd.Cmd):
             emptyline + enter
         """
         pass
+
+    def do_create(self, line):
+        """
+            Creates a new instance of BaseModel,
+            saves it (to the JSON file) and prints
+            the id. Ex: $ create BaseModel
+        """
+        if line is None:
+            print("** class name missing **")
+        elif line != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            my_object = BaseModel()
+            my_object.save()
+            print(my_object.id)
+
+    def do_show(self, line):
+        """
+           Prints the string representation of an
+           instance based on the class name and 
+           id. Ex: $ show BaseModel 1234-1234-1234
+        """
+        line = line.split()
+        len_line = len(line)
+        if len_line == 0:
+            print("** class name missing **")
+        elif line[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len_line == 1:
+            print("** instance id missing **")
+        else:
+            storage.reload()
+            obj_found = False
+            for obj in storage.all().values():
+                if obj.id == line[1]:
+                    print(str(obj))
+                    obj_found = True
+                    break
+            if obj_found == False:
+                print("** no instance found **")
+
+    def do_destroy(self, line):
+        """
+           Deletes an instance based on the class
+           name and id (save the change
+           into the JSON file).
+        """
+        if line is None:
+            print("** class name missing **")
+        elif line != "my_object":
+            print("** class doesn't exist **")
+        elif my_object.id is None:
+            print("** instance id missing **")
+
+
+
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@
     classes
 """
 import uuid
-from datetime import datetime
+import datetime
 from models import storage
 
 
@@ -28,15 +28,26 @@ class BaseModel:
                 be updated every time you change your object
 
         """
-        if kwargs is not None:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+        if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    setatrr(self, key, value)
+                    if key == 'created_at':
+                        date_format = '%Y-%m-%dT%H:%M:%S.%f'
+                        self.created_at = datetime.datetime.strptime(value,
+                                                                     date_format)
+                    elif key == 'updated_at':
+                        date_format = '%Y-%m-%dT%H:%M:%S.%f'
+                        self.updated_at = datetime.datetime.strptime(value,
+                                                                     date_format)
+                    else:
+                        setattr(self, key, value)
+        else:
+            storage.new(self)
 
     def __str__(self):
         """
@@ -51,7 +62,7 @@ class BaseModel:
              updates the public instance attribute updated_at
              with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
         storage.save()
 
     def to_dict(self):
